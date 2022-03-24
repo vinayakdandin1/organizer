ea28a866fa33d3d9ad64bca48383022e7b783644
Vinayak Dandin <48254649+vinayakdandin1@users.noreply.github.com>
2022-03-24 22:26:20 +0100

    add create cohort route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -17,12 +17,11 @@ router.get("/createStudents", isLoggedIn, (req, res) => {
 })
 
 router.post("/createStudents", isLoggedIn, async (req, res) => {
-    let students = req.body.studentNames
+    let students = req.body.studentNames.toLowerCase()
     let studentsArray = students.split(" ")
     studentsArray = studentsArray.map((e) => {
         return e = {"name": e}
     })
-    console.log(studentsArray)
 
     try {
         await Student.insertMany(studentsArray)
@@ -30,9 +29,12 @@ router.post("/createStudents", isLoggedIn, async (req, res) => {
         
     } catch (error) {
         console.error(error)
-    }
+    }   
+})
 
-    
+router.get("/createCohort", isLoggedIn, (req, res) => {
+    const loggedInUser = req.session.user
+    res.render("students/cohort", { loggedInUser })
 })
 
 module.exports = router;
